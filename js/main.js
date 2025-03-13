959776036ca028a6001fdf768d8848673d187325
Yadim <v_kuhlmann@outlook.com>
2025-03-13 21:53:11 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,49 @@
+
+document.addEventListener("DOMContentLoaded", (event) => {
+
+
+    // gsap.to(".hero__text", {
+    //     duration: 1,
+    //     ease: "none",
+    //     x: -200,
+    //     opacity: 1,
+    //     // repeat: -1,
+    //     // rotation: 360,
+    // })
+
+
+
+
+    const text = document.querySelector('.hero__text');
+
+    const splitText = (el) => {
+        el.innerHTML = el.textContent.replace(/(\S*)/g, m => {
+            return `<div class="word">` +
+                m.replace(/(-|#|@)?\S(-|#|@)?/g, "<div class='letter'>$&</div>") +
+                `</div>`;
+        });
+        return el;
+    };
+
+    const split = splitText(text);
+
+    function random(min, max) {
+        return (Math.random() * (max - min)) + min;
+    }
+
+    Array.from(split.querySelectorAll('.letter')).forEach((el, idx) => {
+        TweenMax.from(el, 1.5, {
+            opacity: 0,
+            scale: .1,
+            x: random(-500, 500),
+            y: random(-500, 500),
+            z: random(-500, 500),
+            delay: idx * 0.02,
+            repeat: 0,
+        })
+    });
+
+
+});
+
+
