0dae2ca73bcce0bc62567c3319f8127954dcf3d6
Vadim <my@my.com>
2025-03-19 16:58:04 +0100

    fix

--- js/main.js
+++ js/main.js
@@ -14,34 +14,34 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 
 
-    const text = document.querySelector('.hero__text');
+    // const text = document.querySelector('.hero__text');
 
-    const splitText = (el) => {
-        el.innerHTML = el.textContent.replace(/(\S*)/g, m => {
-            return `<div class="word">` +
-                m.replace(/(-|#|@)?\S(-|#|@)?/g, "<div class='letter'>$&</div>") +
-                `</div>`;
-        });
-        return el;
-    };
+    // const splitText = (el) => {
+    //     el.innerHTML = el.textContent.replace(/(\S*)/g, m => {
+    //         return `<div class="word">` +
+    //             m.replace(/(-|#|@)?\S(-|#|@)?/g, "<div class='letter'>$&</div>") +
+    //             `</div>`;
+    //     });
+    //     return el;
+    // };
 
-    const split = splitText(text);
+    // const split = splitText(text);
 
-    function random(min, max) {
-        return (Math.random() * (max - min)) + min;
-    }
+    // function random(min, max) {
+    //     return (Math.random() * (max - min)) + min;
+    // }
 
-    Array.from(split.querySelectorAll('.letter')).forEach((el, idx) => {
-        TweenMax.from(el, 1.5, {
-            opacity: 0,
-            scale: .1,
-            x: random(-500, 500),
-            y: random(-500, 500),
-            z: random(-500, 500),
-            delay: idx * 0.02,
-            repeat: 0,
-        })
-    });
+    // Array.from(split.querySelectorAll('.letter')).forEach((el, idx) => {
+    //     TweenMax.from(el, 1.5, {
+    //         opacity: 0,
+    //         scale: .1,
+    //         x: random(-500, 500),
+    //         y: random(-500, 500),
+    //         z: random(-500, 500),
+    //         delay: idx * 0.02,
+    //         repeat: 0,
+    //     })
+    // });
 
 
 });
