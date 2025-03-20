ec0552b6434ecb2cd14db17472eecc4c6683c897
Vadim <my@my.com>
2025-03-20 14:42:35 +0100

    fix: animation

--- js/main.js
+++ js/main.js
@@ -14,22 +14,22 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 
 
-    // const text = document.querySelector('.hero__text');
+    const text = document.querySelector('.header__logo');
 
-    // const splitText = (el) => {
-    //     el.innerHTML = el.textContent.replace(/(\S*)/g, m => {
-    //         return `<div class="word">` +
-    //             m.replace(/(-|#|@)?\S(-|#|@)?/g, "<div class='letter'>$&</div>") +
-    //             `</div>`;
-    //     });
-    //     return el;
-    // };
+    const splitText = (el) => {
+        el.innerHTML = el.textContent.replace(/(\S*)/g, m => {
+            return `<div class="word">` +
+                m.replace(/(-|#|@)?\S(-|#|@)?/g, "<div class='letter'>$&</div>") +
+                `</div>`;
+        });
+        return el;
+    };
 
-    // const split = splitText(text);
+    const split = splitText(text);
 
-    // function random(min, max) {
-    //     return (Math.random() * (max - min)) + min;
-    // }
+    function random(min, max) {
+        return (Math.random() * (max - min)) + min;
+    }
 
     // Array.from(split.querySelectorAll('.letter')).forEach((el, idx) => {
     //     TweenMax.from(el, 1.5, {
@@ -43,6 +43,30 @@ document.addEventListener("DOMContentLoaded", (event) => {
     //     })
     // });
 
+       gsap.from(".header__logo .letter", {
+            duration: 2,
+            y: -100,
+            scale: 0,
+            ease: "bounce.out",
+            stagger: {
+                each: 0.1,
+                from: "center"
+            }
+        })
+
+       gsap.from(".header__btn span", {
+            y: -100,
+            duration: .5,
+            delay: 1.5,
+            scaleX: 0,
+            ease: "bounce.in",
+            stagger: {
+                each: .3,
+                from: "center"
+            }
+        })
+
+      
 
 });
 
